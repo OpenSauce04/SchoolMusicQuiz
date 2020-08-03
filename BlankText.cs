fb372251834e83fb1cafb2223d913252f3dad3ac
Daniel Bradbury <danieljamesbradbury@hotmail.com>
2020-08-03 16:53:48 +0100

    Added comments to text blanker

--- BlankText.cs
+++ BlankText.cs
@@ -11,17 +11,18 @@ namespace SchoolCodingThingIDKwhatItsCalled
 	{
 		public static string Parse(string input)
 		{
-			string[] outputArray = new string[15];
+			string[] outputArray = new string[15]; // Create array with space for 15 words.
 			string output;
-			int index = 0;
-			string[] words = input.Split(' ');
+			int index = 0; // Variable used to track how many words have been iterated through.
+			string[] words = input.Split(' '); // Splits the song title (input) into individual words.
 			foreach (string word in words) {
-				string first = word.Substring(0, 1);
-				outputArray[index] = first + new String('_', word.Length-1);
+				string first = word.Substring(0, 1); // Stored first letter of current word as the variable first
+				outputArray[index] = first + new String('_', word.Length-1); // Generates the blanked out word, and stores it in the
+				                                                             // output array to be later turned into a single string
 				index += 1;
 			}
-			output = string.Join(" ", outputArray);
-			return output;
+			output = string.Join(" ", outputArray); // Combine the generated array into a single string
+			return output;                          // ...and return that string.
 		}
 	}
 }
