6768897a179a36550dd400c96db04ca4f4980d9e
Daniel Bradbury <danieljamesbradbury@hotmail.com>
2020-08-03 17:12:53 +0100

    Fixed login system preemptively failing login when username is incorrect

--- Login.cs
+++ Login.cs
@@ -11,8 +11,9 @@ namespace SchoolCodingThingIDKwhatItsCalled
 			Console.Write("Enter your username: ");
 			string user = SHA256.calc(Console.ReadLine()); // Take user input for username and store SHA256 hash of input
 			Console.Write("Enter you password: ");
+			string pass = SHA256.calc(Console.ReadLine());
 			if (File.Exists("userdb/" + user)
-				&& File.ReadAllText("userdb/" + user) == SHA256.calc(Console.ReadLine())) // Checks to see if the user's entry in the user database exists,
+				&& File.ReadAllText("userdb/" + user) == pass) // Checks to see if the user's entry in the user database exists,
 																						  // and if it does checks to see if the user's password hash matches
 																						  // with the user's input.
 			{
